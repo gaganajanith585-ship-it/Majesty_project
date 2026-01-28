b0b6abd186e8819dfe9c7c85ed0f7c4d403f5b30
hasithaea <hasitha2k4@gmail.com>
2026-01-29 00:44:21 +0530

    fixed some bugs encounterd while trying connect php and html

--- sign_up.logic.php
+++ sign_up.logic.php
@@ -4,12 +4,13 @@ if(isset($_POST['submit']))
     {
         $name = $_POST['name'];
         $email = $_POST['email'];
-        $password = $_POST['password'];
         $address = $_POST['address'];
+        $mobile = $_POST['mobile'];
+        $password = $_POST['password'];
         $role = "user";
 
-        $sql = "insert into users(name,email,address,password,role) 
-        values('$name','$email','$password','$address','$role')";
+        $sql = "insert into users(name,email,address,mobile,password,role) 
+        values('$name','$email','$address','$mobile','$password','$role')";
         $result = mysqli_query($conn,$sql);
 
         if(!$result)
