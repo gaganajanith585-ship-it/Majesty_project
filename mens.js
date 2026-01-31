41d7bdfd36b0e8bf37ea5c73b371b8febb9b2214
hasithaea <hasitha2k4@gmail.com>
2026-02-01 03:07:45 +0530

    added js needed for the cart

--- mens.js
+++ mens.js
@@ -7,12 +7,28 @@ const popupPrice = document.getElementById('popupPrice');
 const popupDesc = document.getElementById('popupDesc');
 const closeBtn = document.getElementById('closeBtn');
 
+const cartId =document.getElementById('cart_id');
+const cartName = document.getElementById('cart_name');
+const cartPrice = document.getElementById('cart_price');
+
 cards.forEach(card => {
   card.addEventListener('click', () => {
+
+    const title = card.dataset.title;
+    const priceText = card.dataset.price;
+    const price = priceText.replace('$', '');
+    const img = card.dataset.img;
+    const desc = card.dataset.desc;
+
     popupImg.src = card.dataset.img;
     popupTitle.textContent = card.dataset.title;
     popupPrice.textContent = card.dataset.price;
     popupDesc.textContent = card.dataset.desc;
+
+    cartId.value = title.replace(/\s+/g, '_');
+    cartName.value = title;
+    cartPrice.value = price;
+
     overlay.style.display = 'flex';
   });
 });
